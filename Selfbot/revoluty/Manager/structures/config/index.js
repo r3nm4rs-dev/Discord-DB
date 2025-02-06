b1de3b2250752238a314b5e41195508b7a6f51a4
Renaud Yeti <r3nm4rsss@proton.me>
2025-02-05 23:56:05 -0500

    added revoluty

---
+++ Selfbot/revoluty/Manager/structures/config/index.js
@@ -0,0 +1,31 @@
+module.exports = {
+    manager: {
+        token: '',
+        owner: [
+            {
+                id: '1331242382167117994'
+            }
+        ],
+        botId: "1336855870897782846"
+    },
+    footer: {
+        text: "Pluzio",
+        icon_url: "https://cdn.discordapp.com/attachments/1262809506455490724/1266294391505686559/artworks-wWzJgjKu2IRZBNxO-ZmUvDw-t500x500.jpg?ex=66a4a00d&is=66a34e8d&hm=ee4cc3577153c62c4a61bc0501232e5f8461ed581c46638fffe4881251919b24&"
+    },
+    color: "#3535f8",
+    team: {
+        owner: ["1331242382167117994"],
+        resp: [],
+        market: [],
+        design: [],
+        mod: [],
+        devs: ["1331242382167117994"],
+        tech: []
+    },
+    prefix: "-",
+    support: 'https://discord.gg/Hp4UTnkEf2',
+    buyer: "1331242382167117994",
+    sharding: false,
+    defaultLang: "fr"
+
+}
